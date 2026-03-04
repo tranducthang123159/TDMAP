1a77671048ffcf127f537dd2d3549e754ac2d71c
Tran Duc Thang <emailgithubcuaban@gmail.com>
2026-03-04 23:31:17 +0700

    het

--- TDMAP/resources/views/admin/giao_dien/index.blade.php
+++ TDMAP/resources/views/admin/giao_dien/index.blade.php
@@ -6,15 +6,21 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <!-- Bootstrap CSS -->
+
+
+
+
+
+<link rel="stylesheet" href="{{ asset('ad/assets/vendor/fonts/material-design-iconic-font/css/materialdesignicons.min.css') }}">
+<link rel="stylesheet" href="{{ asset('ad/assets/vendor/charts/c3charts/c3.css') }}">
 <link rel="stylesheet" href="{{ asset('ad/assets/vendor/bootstrap/css/bootstrap.min.css') }}">
 <link rel="stylesheet" href="{{ asset('ad/assets/vendor/fonts/circular-std/style.css') }}">
 <link rel="stylesheet" href="{{ asset('ad/assets/libs/css/style.css') }}">
 <link rel="stylesheet" href="{{ asset('ad/assets/vendor/fonts/fontawesome/css/fontawesome-all.css') }}">
 <link rel="stylesheet" href="{{ asset('ad/assets/vendor/charts/chartist-bundle/chartist.css') }}">
-<link rel="stylesheet" href="{{ asset('ad/assets/vendor/charts/morris-bundle/morris.css') }}">
-<link rel="stylesheet" href="{{ asset('ad/assets/vendor/fonts/material-design-iconic-font/css/materialdesignicons.min.css') }}">
-<link rel="stylesheet" href="{{ asset('ad/assets/vendor/charts/c3charts/c3.css') }}">
+
 <link rel="stylesheet" href="{{ asset('ad/assets/vendor/fonts/flag-icon-css/flag-icon.min.css') }}">
+<link rel="stylesheet" href="{{ asset('ad/assets/vendor/charts/morris-bundle/morris.css') }}">
     <title>Concept - Bootstrap 4 Admin Dashboard Template</title>
 </head>
 
@@ -989,26 +995,23 @@
     <!-- ============================================================== -->
     <!-- Optional JavaScript -->
     <!-- jquery 3.3.1 -->
-   <script src="{{ asset('ad/assets/vendor/jquery/jquery-3.3.1.min.js') }}"></script>
 
-<script src="{{ asset('ad/assets/vendor/bootstrap/js/bootstrap.bundle.js') }}"></script>
-
-<script src="{{ asset('ad/assets/vendor/slimscroll/jquery.slimscroll.js') }}"></script>
-
-<script src="{{ asset('ad/assets/libs/js/main-js.js') }}"></script>
-
-<script src="{{ asset('ad/assets/vendor/charts/chartist-bundle/chartist.min.js') }}"></script>
 
 <script src="{{ asset('ad/assets/vendor/charts/sparkline/jquery.sparkline.js') }}"></script>
-
 <script src="{{ asset('ad/assets/vendor/charts/morris-bundle/raphael.min.js') }}"></script>
 <script src="{{ asset('ad/assets/vendor/charts/morris-bundle/morris.js') }}"></script>
-
 <script src="{{ asset('ad/assets/vendor/charts/c3charts/c3.min.js') }}"></script>
 <script src="{{ asset('ad/assets/vendor/charts/c3charts/d3-5.4.0.min.js') }}"></script>
 <script src="{{ asset('ad/assets/vendor/charts/c3charts/C3chartjs.js') }}"></script>
-
 <script src="{{ asset('ad/assets/libs/js/dashboard-ecommerce.js') }}"></script>
+   <script src="{{ asset('ad/assets/vendor/jquery/jquery-3.3.1.min.js') }}"></script>
+<script src="{{ asset('ad/assets/vendor/bootstrap/js/bootstrap.bundle.js') }}"></script>
+<script src="{{ asset('ad/assets/vendor/slimscroll/jquery.slimscroll.js') }}"></script>
+<script src="{{ asset('ad/assets/libs/js/main-js.js') }}"></script>
+<script src="{{ asset('ad/assets/vendor/charts/chartist-bundle/chartist.min.js') }}"></script>
+
+
+
+
 </body>
- 
 </html>
