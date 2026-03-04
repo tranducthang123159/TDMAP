834fbd09c32c7d676d75aab8268a7a1113e590ea
Tran Duc Thang <emailgithubcuaban@gmail.com>
2026-03-04 17:03:30 +0700

    update

--- TDMAP/resources/views/auth/login.blade.php
+++ TDMAP/resources/views/auth/login.blade.php
@@ -5,7 +5,6 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Hệ thống quản lý địa chính</title>
 
-@vite(['resources/css/app.css','resources/js/app.js'])
 
 <style>
 *{box-sizing:border-box;}
