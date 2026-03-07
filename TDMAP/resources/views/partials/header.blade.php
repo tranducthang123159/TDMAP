c1b7d15d5f34852902a69946918d08863bd6b0e9
Tran Duc Thang <emailgithubcuaban@gmail.com>
2026-03-07 11:05:02 +0700

    đ

--- TDMAP/resources/views/partials/header.blade.php
+++ TDMAP/resources/views/partials/header.blade.php
@@ -1,4 +1,4 @@
-<link rel="stylesheet" href="{{ asset('css/header.css') }}">
+<link rel="stylesheet" href="{{ asset('css/header.css') }}?v={{ time() }}">
 
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
 <!-- MENU SLIDE -->
